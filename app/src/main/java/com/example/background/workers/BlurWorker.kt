a07d3f259f85a5109990dc07ea1855fcdddec173
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-07-20 14:59:13 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,45 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+    override fun doWork(): Result {
+        val appContext = applicationContext
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        makeStatusNotification("Blurring image", appContext)
+
+        return try {
+            /*val picture =
+                BitmapFactory.decodeResource(appContext.resources, R.drawable.android_cupcake)*/
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e("MYTAG", "Invalid input Uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture =
+                BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)))
+
+            val output = blurBitmap(picture, appContext)
+
+            // bitmap untuk file
+            val outputUri = writeBitmapToFile(appContext, output)
+
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(outputData)
+        } catch (throwable: Throwable) {
+            Log.e("MYTAG", "Error applying blur")
+            throwable.printStackTrace()
+            Result.failure()
+        }
+    }
+}
